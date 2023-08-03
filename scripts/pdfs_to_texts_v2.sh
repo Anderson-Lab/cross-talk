4e24e6ad0afba323a5dcfc36c19be54664b73649
Paul Anderson <pauleanderson@gmail.com>
2023-08-03 21:46:06 +0000

    update

--- scripts/pdfs_to_texts_v2.sh
+++ scripts/pdfs_to_texts_v2.sh
@@ -9,8 +9,9 @@ OUTDIR=$2
 for file in `ls "$DIR" | grep '\.pdf$'`; do
     echo "[$file]"
     [ -d "$DIR/../$OUTDIR/" ] || mkdir "$DIR/../$OUTDIR/"
+    echo cp "$DIR/$file" tmp/source.pdf
     cp "$DIR/$file" tmp/source.pdf && \
-    if docker run --network host --env OPENAI_API_KEY=$OPENAI_API_KEY -v $PWD/pyknowledgegraph:/app/pyknowledgegraph -v $PWD/scripts:/app/scripts -v $PWD/tmp:/app/tmp cross-talk python3 ./scripts/pdf_to_text_v2.py tmp/source.pdf tmp/source.json $file; then
+    if docker run --workdir /app --network host --env OPENAI_API_KEY=$OPENAI_API_KEY -v $PWD/pyknowledgegraph:/app/pyknowledgegraph -v $PWD/scripts:/app/scripts -v $PWD/tmp:/app/tmp cross-talk python3 /app/scripts/pdf_to_text_v2.py tmp/source.pdf tmp/source.json $file; then
       cp tmp/source.json "$DIR/../$OUTDIR/$file.json" && echo "Completed: $file"
     else
       echo "Failed: $file"
